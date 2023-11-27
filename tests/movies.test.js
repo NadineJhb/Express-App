0a20cf1ad2ace1343ee40e1a7f5b32d6e564d720
Nadine <nadine.motel@gmail.com>
2023-11-27 11:14:22 +0100

    exo TDD

---
+++ tests/movies.test.js
@@ -0,0 +1,27 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /api/movies", () => {
+  it("should return all movies", async () => {
+    const response = await request(app).get("/Api/movies");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
+
+describe("GET /api/users/:id", () => {
+  it("should return one user", async () => {
+    const response = await request(app).get("/api/users/1");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+
+    expect(response.status).toEqual(200);
+  });
+
+  it("should return no user", async () => {
+    const response = await request(app).get("/api/users/0");
+
+    expect(response.status).toEqual(404);
+  });
+});
